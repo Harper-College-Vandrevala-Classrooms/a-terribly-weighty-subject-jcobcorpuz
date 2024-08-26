2c77cf22d4219f3366f905361577b36c8145b2f3
jcobcorpuz <cc62200@mail.harpercollege.edu>
2024-08-25 21:52:56 -0500

    Converted oz to lbs and oz to lbs&oz

--- src/main/java/com/csc/Converter.java
+++ src/main/java/com/csc/Converter.java
@@ -3,10 +3,31 @@ package com.csc;
 public class Converter {
 
   public String toPounds(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPounds'");
+    double pounds = ounces / 16.0;
+    String unit;
+
+    if (pounds == 1.0)
+    {
+      unit = "lb";
+    }
+    else {
+      unit = "lbs";
+    }
+    return String.format("%.4f %s", pounds, unit);
   }
 
   public String toPoundsAndOunces(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPoundsAndOunces'");
+    int pounds = ounces / 16;
+
+    int remainder = ounces % 16;
+
+    String unit2;
+    if (pounds == 1){
+      unit2 = "lb";
+    }
+    else{
+      unit2 = "lbs";
+    }
+    return pounds + " " + unit2 + " " + remainder + " oz";
   }
 }
